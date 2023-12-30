c15788ae222278e557671f74c98f143283dd669a
Yric <yroslavovebo@gmail.com>
2023-12-30 14:55:02 +0300

    fast commit

--- manager_Tools.py
+++ manager_Tools.py
@@ -4,29 +4,74 @@ from pygame import Surface
 from pygame import Rect
 
 class Button(Sprite):
-    def __init__(self, text, issue=lambda: None, path=''):
+    def __init__(self, text, issue=lambda: None, picture_path=None, alternative_picture_path=None):
         super().__init__()
+        self.issue = issue
         self.text = text
         self.is_active = None
-        self.issue = issue
+        self.picture_path = picture_path
+        self.alternative_picture_path = alternative_picture_path
         self.rect = Rect(0, 0, 100, 100)
         self.image = Surface(self.rect.size)
+        self.add_buttton_picture(self.picture_path)
+        self.render(self.image)
 
-    def resize(self, heigth, width):
+    def resize(self, size):
         self.rect.width, self.height = size
 
-    def replace(self, x, y):
+    def replace(self, pos):
         self.rect.x, self.rect.y = pos
 
+    def on_click(self):
+        if self.issue:
+            self.issue()
     def connect_issue(self, foo):
         self.issue = foo
+
+    def add_buttton_picture(self, path):
+        if path:
+            self.picture_path = pygame.image.load(path)
+
+    def add_alternative_picture(self, path):
+        if path:
+            self.alternative_picture_path = pygame.image.load(path)
 
     def update(self, *args):
         if not args:
             return
 
-        if args[0].type == pygame.MOUSEBUTTONDOWN:
+        if args[0].type == pygame.MOUSEMOTION:
             if self.rect.collidepoint(args[0].pos):
+                 if not self.is_active:
+                      self.is_active = True
+                      self.render(self.image)
+
+            else:
+                if self.is_active:
+                    self.is_active = False
+                    self.render(self.image)
+        elif args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            self.on_click()
+
+    def render(self, screen):
+        if self.picture_path:
+            if self.alternative_picture_path:
+                if self.is_active:
+                    screen.blit(self.alternative_picture_path)
+                else:
+                    screen.blit(self.picture_path)
+            else:
+                screen.blit(self.picture_path)
+        else:
+            if self.is_active:
+                color = (200, 100, 100)
+            else:
+                color = (100, 200, 100)
+            pygame.draw.rect(screen, color, (self.rect.width, self.rect.height), self.rect.size)
+
+
+
+
 
 
 class RaangeSlider(Sprite):
