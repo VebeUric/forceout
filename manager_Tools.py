d6de9223d77ed74735c92f6c6fd40157ad974271
Yric <yroslavovebo@gmail.com>
2024-01-14 14:00:17 +0300

    thirrd commit: finish button class

--- manager_Tools.py
+++ manager_Tools.py
@@ -4,11 +4,13 @@ from pygame import Surface
 from pygame import Rect
 
 class Button(Sprite):
-    def __init__(self, text, issue=lambda: None, picture_path=None, alternative_picture_path=None):
+    def __init__(self, text=None, issue=lambda: None, picture_path=None, alternative_picture_path=None):
         super().__init__()
         self.issue = issue
         self.text = text
+        self.text_size = 50
         self.is_active = None
+        self.color = BLACK
         self.picture_path = picture_path
         self.alternative_picture_path = alternative_picture_path
         self.rect = Rect(0, 0, 100, 100)
@@ -16,11 +18,18 @@ class Button(Sprite):
         self.add_buttton_picture(self.picture_path)
         self.render(self.image)
 
+    def resize_text(self, new_size):
+        self.text_size = new_size
+
+
     def resize(self, size):
         self.rect.width, self.height = size
 
     def replace(self, pos):
         self.rect.x, self.rect.y = pos
+
+    def make_new_text_color(self, new_color):
+        self.color = new_color
 
     def on_click(self):
         if self.issue:
@@ -54,6 +63,7 @@ class Button(Sprite):
             self.on_click()
 
     def render(self, screen):
+        self.button_rect = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
         if self.picture_path:
             if self.alternative_picture_path:
                 if self.is_active:
@@ -68,6 +78,14 @@ class Button(Sprite):
             else:
                 color = (100, 200, 100)
             pygame.draw.rect(screen, color, (self.rect.width, self.rect.height), self.rect.size)
+        self.show_text()
+
+    def show_text(self):
+        if self.text:
+            font = pygame.font.Font(None, self.text_size)                                        # Сделать обособленную фуекцию чтобы можно было скрывать текст
+            text_surface = font.render(self.text, True, self.color)
+            text_rect = text_surface.get_rect(center=self.buttob_rect.center)
+            self.image.blit(text_rect, text_rect)
 
 
 
@@ -75,8 +93,9 @@ class Button(Sprite):
 
 
 class RaangeSlider(Sprite):
-    def __init__(self, text, min=0, max=100):
+    def __init__(self, text, min=0, max=100, issue = lambda: None):
         super().__init__()
+        self.text = text()
 
     def init_button(self):
         pass
